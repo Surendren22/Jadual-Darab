5a1cf3a73fb49207709091f803186969ea1a12a8
SURENDREN <127081472+Surendren22@users.noreply.github.com>
2023-06-12 03:55:28 +0800

    index.php

--- index.php
+++ index.php
@@ -1,35 +1,23 @@
+<?php
+function calculateArea($length, $width) {
+  $area = $length * $width;
+  return $area;
+}
+
+$length = 5;
+$width = 10;
+$area = calculateArea($length, $width);
+?>
+
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercise</title>
+  <title>Area Calculation</title>
 </head>
-
 <body>
-    <h3>SIFIR</h3>
-    <form method="POST">
-        <label for="num">Nombor sifir:</label>
-        <input type="number" name="num">
-        <button type="submit" name="kiraan">Kiraan</button>
-        <p></p>
-    </form>
-
-    <?php
-    if (isset($_POST['kiraan'])) {
-        $num = intval($_POST['num']);
-        if ($num >= 1 && $num <= 20) {
-          echo "<table>";
-          for ($i = 1; $i <= 20; $i++) {
-            $result = $i * $num;
-            echo "<tr><td>{$i} x {$num}</td><td> = {$result}</td></tr>";
-          }
-          echo "</table>";
-        } 
-      }
-    ?>
+  <h1>Area Calculation</h1>
+  <p>Length: <?php echo $length; ?></p>
+  <p>Width: <?php echo $width; ?></p>
+  <p>Area: <?php echo $area; ?></p>
 </body>
-
 </html>
