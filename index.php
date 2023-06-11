e30aa92aec7e312636c49b5abd50dcb7f64f9a85
SURENDREN <127081472+Surendren22@users.noreply.github.com>
2023-06-12 03:56:11 +0800

    index.php

--- index.php
+++ index.php
@@ -21,3 +21,42 @@ $area = calculateArea($length, $width);
   <p>Area: <?php echo $area; ?></p>
 </body>
 </html>
+
+
+
+<?php
+$array = array(
+  array(
+    'Item 1',
+    array('Value 1', 'Value 2', 'Value 3', 'Value 4')
+  ),
+  array(
+    'Item 2',
+    array('Value 1', 'Value 2')
+  )
+);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Multidimensional Array</title>
+</head>
+<body>
+  <h1>Multidimensional Array</h1>
+  <table>
+    <?php foreach ($array as $item) : ?>
+      <tr>
+        <th><?php echo $item[0]; ?></th>
+        <td>
+          <ul>
+            <?php foreach ($item[1] as $value) : ?>
+              <li><?php echo $value; ?></li>
+            <?php endforeach; ?>
+          </ul>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+</body>
+</html>
